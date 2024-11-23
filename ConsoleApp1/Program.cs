5f357aad5e264aa1a101395e3f5ee2e02a7214f9
Andrey <am06041102@gmail.com>
2024-11-23 12:36:32 +0300

    Add password encryption and decryption functionality

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿static string EncryptPassword(string password)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            using Aes aes = Aes.Create();
+            aes.Key = GetEncryptionKey();
+            aes.IV = new byte[16]; // Нулевой IV для простоты
+
+            using var encryptor = aes.CreateEncryptor();
+            byte[] encryptedBytes = encryptor.TransformFinalBlock(passwordBytes, 0, passwordBytes.Length);
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        static string DecryptPassword(string encryptedPassword)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedPassword);
+            using Aes aes = Aes.Create();
+            aes.Key = GetEncryptionKey();
+            aes.IV = new byte[16]; // Нулевой IV для простоты
+
+            using var decryptor = aes.CreateDecryptor();
+            byte[] passwordBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+            return Encoding.UTF8.GetString(passwordBytes);
+        }
